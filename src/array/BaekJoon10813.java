633613204f5c9b30b7057009f4347f343a99e475
bea0266 <bea0266@naver.com>
2024-07-02 00:51:53 +0900

    백준 단계별 문제풀이(배열) 10813 - 공 바꾸기 속도 향상 및 메모리 절약을 위해 temp, start, end 선언 바깥으로 뺌

--- src/array/BaekJoon10813.java
+++ src/array/BaekJoon10813.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class BaekJoon10813 {
     /**
      * 백준 10813번 공 바꾸기 문제
-     * 임시 변수 (int t) 활용
-     * 시작 지점(int start)을 증가시키고 끝 지점(int end)를 감소시키면서 배열의 start, end 수의 인덱스를 교체한다.
+     * 임시 변수 temp 활용하여 i번 바구니와 j번 바구니의 공 교체
      */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int m = s.nextInt();
+        int temp, start, end;
 
         int[] baguni = new int[n];
 
@@ -20,9 +20,9 @@ public class BaekJoon10813 {
         }
 
         for (int i = 0; i < m; i++) {
-            int start = s.nextInt() - 1;
-            int end = s.nextInt() - 1;
-            int temp = baguni[start];
+            start = s.nextInt() - 1;
+            end = s.nextInt() - 1;
+            temp = baguni[start];
             baguni[start] = baguni[end];
             baguni[end] = temp;
         }
